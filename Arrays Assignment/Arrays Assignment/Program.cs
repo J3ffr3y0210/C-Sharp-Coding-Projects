12392049aa91a85f8ee1db16ec4209fde15a6ddf
J3ffr3y0210 <jerhine16@gmail.com>
2022-05-26 18:18:23 -0700

    commit

---
+++ Arrays Assignment/Arrays Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        int[] numArray = new int[] { 0, 1, 2, 3, 4 };
+
+        Console.WriteLine(numArray[2]);
+        Console.ReadLine();
+
+        string[] numArray1 = new string[] { "Jeff", "Joe", "Josh", "John", "Jake" };
+
+        numArray1[4] = "Paul";
+
+        Console.WriteLine(numArray1[4]);
+        Console.ReadLine();
+
+        List<string> intList = new List<string>();
+        intList.Add("Apple");
+        intList.Add("Mango");
+        intList.Add("Orange");
+
+        Console.WriteLine(intList[2]);
+        Console.ReadLine();
+            
+    }
+}
